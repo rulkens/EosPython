94e80c2d19667de45502b7c02b558d709df2404e
Alexander Rulkens <rulkens@gmail.com>
2015-04-02 19:12:01 +0200

    moved the generic interface to api.py, updated the driver with status info, created a webserver with Flask

---
+++ api.py
@@ -0,0 +1,72 @@
+from lib.Eos_Driver import EOS_Driver
+
+# ===========================================================================
+# Simplified EOS api
+# ===========================================================================
+
+# create the Eos_Driver object
+eos = EOS_Driver()
+
+# functions
+def allOff(opts):
+    eos.allOff()
+    return "all lights turned off"
+def allOn(opts):
+    eos.allOn()
+    return "all lights turned on"
+def one(opts):
+    eos.one(int(opts[0]), float(opts[1]))
+    return "light %s turned to intensity %s" % (opts[0], opts[1])
+def set(opts):
+    # convert opts to a list of floats
+    eos.set(map(lambda o: float(o), opts))
+    return "lights set to" % opts
+def all(opts):
+    eos.all(float(opts[0]))
+    return "set all lights to intensity %s" % opts[0]
+def only(opts):
+    eos.only(int(opts[0]), float(opts[1]))
+    return "turn on only light %s to intensity %s" % (opts[0],opts[1])
+def on(opts=None):
+    if opts != None and opts != []:
+        eos.on(int(opts[0]))
+        return "light %s turned on" % opts[0]
+    else:
+        eos.allOn()
+        return "all lights turned on"
+def off(opts=None):
+    if opts != None and opts != []:
+        eos.off(int(opts[0]))
+        return "light %s turned off" % opts[0]
+    else:
+        eos.allOff()
+        return "all lights turned off"
+def setFreq(opts):
+    eos.setFreq(int(opts[0]))
+    return "setting a new frequency to " % opts[0]
+
+def status(opts):
+    return eos.getStatus()
+
+# overview of all the actions possible
+actions = {
+    'alloff':       allOff,
+    'allon':        allOn,
+    'one':          one,
+    'set':          set,
+    'all':          all,
+    'only':         only,
+    'on':           on,
+    'off':          off,
+    'setfreq':      setFreq,
+    'status':       status
+}
+
+def errorHandler():
+    print "the action cannot be found"
+
+# export function
+def EOS_API(action, args=None):
+    print args
+    return {'result': actions.get(action, errorHandler)(args), 'status': eos.getStatus() }
+
---
+++ server.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+# ===========================================================================
+# default server for communicating with the EOS from outside
+# ===========================================================================
+
+from flask import Flask, jsonify, render_template
+from api import EOS_API
+import datetime
+
+# create the APP
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    now = datetime.datetime.now()
+    timeString = now.strftime("%Y-%m-%d %H:%M")
+    templateData = {
+      'title' : 'HELLO!',
+      'time': timeString
+    }
+    return render_template('main.html', **templateData)
+    
+@app.route("/api/<action>", defaults={'args':''})
+@app.route("/api/<action>/<args>")
+def apiHandler(action, args=''):
+    if action == None:
+        return jsonify({'error': 'specify an action'})
+    else:
+        # execute the action
+        return jsonify({'result': EOS_API(action, args.split(','))})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5151, debug=True)
